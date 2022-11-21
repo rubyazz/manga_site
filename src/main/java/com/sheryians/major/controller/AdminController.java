efc2cd74c8b41e507f421f829c55dfe3fe595805
rubyazz <ersultan.abduvalov@gmail.com>
2022-11-22 01:56:49 +0600

    project done

--- src/main/java/com/sheryians/major/controller/AdminController.java
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -2,20 +2,25 @@ package com.sheryians.major.controller;
 
 import com.sheryians.major.dto.ProductDTO;
 import com.sheryians.major.model.Category;
+import com.sheryians.major.model.Product;
 import com.sheryians.major.service.CategoryService;
 import com.sheryians.major.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.Multipart;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Controller
 public class AdminController {
+    public static String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/productImages";
     @Autowired
     CategoryService categoryService;
     @Autowired
@@ -67,6 +72,30 @@ public class AdminController {
         model.addAttribute("productDTO", new ProductDTO());
         model.addAttribute("categories", categoryService.getAllCategory());
         return "productsAdd";
+    }
+    @PostMapping("/admin/products/add")
+    public String productAddPost(@ModelAttribute("productDTO")ProductDTO productDTO,
+                                 @RequestParam("productImage")MultipartFile file,
+                                 @RequestParam("imgName")String imgName)throws IOException{
+        Product product = new Product();
+        product.setId(productDTO.getId());
+        product.setName(productDTO.getName());
+        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+        product.setPrice(productDTO.getPrice());
+        product.setWeight(productDTO.getWeight());
+        product.setDescription(productDTO.getDescription());
+        String imageUUID;
+        if(!file.isEmpty()){
+            imageUUID = file.getOriginalFilename();
+            Path fileNameAndPath = Paths.get(uploadDir, imageUUID);
+            Files.write(fileNameAndPath, file.getBytes());
+        }else{
+            imageUUID = imgName;
+        }
+        product.setImageName(imageUUID);
+        productService.addProduct(product);
+
+        return "redirect:/admin/products";
     }
 
 }
---
+++ src/main/java/com/sheryians/major/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.sheryians.major.controller;
+
+import com.sheryians.major.model.Product;
+import com.sheryians.major.service.CategoryService;
+import com.sheryians.major.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductService productService;
+
+    @GetMapping({"/", "shop"})
+    public String shop(Model model){
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProduct());
+        return "shop";
+    }
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(Model model, @PathVariable int id){
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductsByCategoryId(id));
+        return "shop";
+    }
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProduct(Model model, @PathVariable int id){
+        model.addAttribute("product", productService.getProductById(id).get());
+        return "viewProduct";
+    }
+}
